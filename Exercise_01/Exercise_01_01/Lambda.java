c9d6da13fcd68fcf21377071dd983e904bba2ad9
AminRana <57844730+AminRana@users.noreply.github.com>
2024-11-24 20:33:12 +0000

    Update Lambda.java

--- Exercise_01/Exercise_01_01/Lambda.java
+++ Exercise_01/Exercise_01_01/Lambda.java
@@ -1,10 +1,30 @@
-/* (Display three messages) Write a program that displays Welcome to Java,
-   Welcome to Computer Science, and Programming is fun.
-*/
-public class Exercise_01_01 {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Java");
-		System.out.println("Welcome to Computer Science");
-		System.out.println("Programming is fun");
-	}
+// Java program to demonstrate lambda expressions
+// to implement a user defined functional interface.
+
+// A sample functional interface (An interface with
+// single abstract method
+interface FuncInterface
+{
+    // An abstract function
+    void abstractFun(int x);
+
+    // A non-abstract (or default) function
+    default void normalFun()
+    {
+       System.out.println("Hello");
+    }
+}
+
+class Test
+{
+    public static void main(String args[])
+    {
+        // lambda expression to implement above
+        // functional interface. This interface
+        // by default implements abstractFun()
+        FuncInterface fobj = (int x)->System.out.println(2*x);
+
+        // This calls above lambda expression and prints 10.
+        fobj.abstractFun(5);
+    }
 }
