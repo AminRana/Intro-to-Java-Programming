58da6ca5b135be47fb8696f86747f55c1a983815
AminRana <57844730+AminRana@users.noreply.github.com>
2024-11-24 21:04:00 +0000

    Update Lambda2.java

--- Exercise_01/Exercise_01_01/Lambda2.java
+++ Exercise_01/Exercise_01_01/Lambda2.java
@@ -1,10 +1,25 @@
-/* (Display three messages) Write a program that displays Welcome to Java,
-   Welcome to Computer Science, and Programming is fun.
-*/
-public class Exercise_01_01 {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Java");
-		System.out.println("Welcome to Computer Science");
-		System.out.println("Programming is fun");
-	}
+// A Java program to demonstrate simple lambda expressions
+import java.util.ArrayList;
+class Lambda2 {
+    public static void main(String args[])
+    {
+        // Creating an ArrayList with elements
+        // {1, 2, 3, 4}
+        ArrayList<Integer> arrL = new ArrayList<Integer>();
+        arrL.add(1);
+        arrL.add(2);
+        arrL.add(3);
+        arrL.add(4);
+
+        // Using lambda expression to print all elements
+        // of arrL
+        arrL.forEach(n -> System.out.println(n));
+
+        // Using lambda expression to print even elements
+        // of arrL
+        arrL.forEach(n -> {
+            if (n % 2 == 0)
+                System.out.println(n);
+        });
+    }
 }
